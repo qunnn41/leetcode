61d12df1af0620ba107906583af902e2cd6b05ac
Yiqun Wang <wyqun_work@163.com>
2015-12-08 22:38:20 +0800

    12.8

---
+++ src/answer/BurstBallon.java
@@ -0,0 +1,33 @@
+package answer;
+
+public class BurstBallon {
+    /**
+     * https://leetcode.com/problems/burst-balloons/
+     */
+    public int maxCoins(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+        int[] Num = new int[nums.length + 2];
+        int n = 1;
+        for (int x : nums)
+            Num[n ++] = x;
+        Num[0] = Num[n ++] = 1;
+
+        int[][]coin = new int[n][n];
+        for (int k = 2; k < n; ++k) {
+            for (int left = 0; left < n - k; ++left) {
+                int right = left + k;
+                for (int i = left + 1; i < right; ++i) {
+                    coin[left][right] = Math.max(coin[left][right],
+                            coin[left][i] + coin[i][right] + Num[left] * Num[i] * Num[right]);
+                }
+            }
+        }
+
+        return coin[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new BurstBallon().maxCoins(new int[] {3,1,5,8}));
+    }
+}
--- src/answer/MinimumHeightTrees.java
+++ src/answer/MinimumHeightTrees.java
@@ -1,7 +1,5 @@
 package answer;
 
-import sun.util.resources.cldr.zh.CalendarData_zh_Hans_HK;
-
 import java.util.*;
 
 public class MinimumHeightTrees {
@@ -9,77 +7,38 @@ public class MinimumHeightTrees {
      * https://leetcode.com/problems/minimum-height-trees/
      */
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        Map<Integer, ArrayList<Integer>> neighbor = new HashMap<Integer, ArrayList<Integer>>();
-        Map<Integer, Integer> minHeight = new HashMap<Integer, Integer>();
-        for (int i = 0; i < n; ++i) {
-            neighbor.put(i, new ArrayList<Integer>());
-        }
-        for (int i = 0; i < edges.length; ++i) {
-            int one = edges[i][0];
-            int two = edges[i][1];
-            neighbor.get(one).add(two);
-            neighbor.get(two).add(one);
+        if (n == 1)
+            return Collections.singletonList(0);
+        Map<Integer, List<Integer>> neighbor = new HashMap<Integer, List<Integer>>();
+        List<Integer> neighborNumber = new LinkedList<Integer>();
+        int[] number = new int[n];
+        for (int i = 0; i < n; ++i)
+            neighbor.put(i, new LinkedList<Integer>());
+
+        for (int[] edge : edges) {
+            neighbor.get(edge[0]).add(edge[1]);
+            neighbor.get(edge[1]).add(edge[0]);
+            number[edge[0]] ++;
+            number[edge[1]] ++;
         }
 
-        for (int root : neighbor.keySet()) {
-            int height = 0;
-            Queue<Integer> queue = new LinkedList<Integer>();
-            HashSet<Integer> set = new HashSet<Integer>();
-            queue.add(root);
-            set.add(root);
-            while (!queue.isEmpty()) {
-                height ++;
+        List<Integer> leaves = new LinkedList<Integer>();
+        for (int i = 0; i < n; ++i)
+            if (number[i] == 1)
+                leaves.add(i);
 
-                ArrayList<Integer> current = new ArrayList<Integer>();
-                while (!queue.isEmpty()) {
-                    current.add(queue.poll());
+        while (n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new LinkedList<Integer>();
+            for (int i : leaves) {
+                for (int j : neighbor.get(i)) {
+                    if(-- number[j] == 1)
+                        newLeaves.add(j);
                 }
-
-                ArrayList<Integer> adding = new ArrayList<Integer>();
-                for (int c : current) {
-                    ArrayList<Integer> neighborhood = neighbor.get(c);
-                    boolean flag = false;
-                    for (int node : neighborhood) {
-                        if (!set.contains(node)) {
-                            set.add(node);
-                            adding.add(node);
-                            flag = true;
-                        }
-                    }
-
-                    //find a leaf
-                    if (!flag) {
-                        if (!minHeight.containsKey(root)) {
-                            minHeight.put(root, height);
-                            break;
-                        }
-                    }
-                }
-                queue.addAll(adding);
             }
-        }
 
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        int min = Integer.MAX_VALUE;
-        for (int key : minHeight.keySet()) {
-            if (min > minHeight.get(key)) {
-                result.clear();
-                result.add(key);
-                min = minHeight.get(key);
-            } else if (min == minHeight.get(key)) {
-                result.add(key);
-            }
+            leaves = newLeaves;
         }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        int [][]edges = new int[5][2];
-        edges[0] = new int[]{0,3};
-        edges[1] = new int[]{1,3};
-        edges[2] = new int[]{2,3};
-        edges[3] = new int[]{4,3};
-        edges[4] = new int[]{5,4};
-        System.out.println(new MinimumHeightTrees().findMinHeightTrees(6, edges));
+        return leaves;
     }
 }
