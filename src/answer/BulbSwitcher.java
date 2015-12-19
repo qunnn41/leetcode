152fcf7a180b968716c44b760c979ac263d228bb
YiQun Wang <YiQun Wang>
2015-12-19 14:22:46 +0800

    12.19

---
+++ src/answer/BulbSwitcher.java
@@ -0,0 +1,10 @@
+package answer;
+
+public class BulbSwitcher {
+    /**
+     * https://leetcode.com/problems/bulb-switcher/
+     */
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt((double) n);
+    }
+}
